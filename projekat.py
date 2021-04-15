65f57b60da08d91fc6274de6dedd165e86a8d473
mladyan <mladjan@mladjan.net>
2021-04-15 18:40:55 +0200

    Update 2 - Push, Log, Selectversion update
    Finished basic functionalities.

--- projekat.py
+++ projekat.py
@@ -2,14 +2,14 @@ import os
 import shutil
 import log
 
-if not os.path.exists('previousver'):
-    os.makedirs('previousver')
+if not os.path.exists('previousver/.'):
+    os.makedirs('previousver/.')
 
-if not os.path.exists('currentver'):
-    os.makedirs('currentver')
+if not os.path.exists('currentver/.'):
+    os.makedirs('currentver/.')
 
-if not os.path.exists('logver.txt'):
-    f = open("logver.txt", "w")
+if not os.path.exists('./vercontrol/logver.txt'):
+    f = open("./vercontrol/logver.txt", "w")
     f.write("0")
     f.close()
 
@@ -18,30 +18,69 @@ def main():
     while x!="0":
         x = input()
 
-        if x == "list":
+        if x == "list":                                 #current directory i njegovi fajlovi, za debug
             print(os.getcwd())
             files = [f for f in os.listdir('.') if os.path.isfile(f)]
             for f in files:
                 print(f)
-                
-        if x == "chgdir":
-            os.chdir('./previousver')
-            print(os.getcwd())
 
+        if x == "log":
+            os.chdir('./vercontrol')
+            f = open('logver.txt', 'r')
+            file_contents = f.read()
+            print ("Prikazuje se broj trenutne verzije i sta je uradjeno u proslim")
+            print (file_contents)
+            f.close()
+            path_parent = os.path.dirname(os.getcwd())  #vracanje jedan dir iznad
+            os.chdir(path_parent)                       #vracanje jedan dir iznad
+                
         if x == "push":
-            print("Unesite sta ste uradili u ovoj verziji")
+            print("Unesite sta ste uradili u ovoj verziji") #saljemo poruku commita
             a = input()
-            log.log_it(a)
-            print(log.vernumbstring)
-            dirpath = os.path.join('./previousver/', log.vernumbstring)
+            os.chdir('./vercontrol')
+            log.log_it(a)                               #ubelezavamo commit, i povecavamo verzijju
+
+            path_parent = os.path.dirname(os.getcwd())  #vracanje jedan dir iznad
+            os.chdir(path_parent)                       #vracanje jedan dir iznad
+
+            dirpath = os.path.join('./previousver', log.vernumbstring)
             if not os.path.exists(dirpath):
                 os.makedirs(dirpath)
-            src = './'
-            dest = dirpath
-            files = os.listdir(src)
+            src = './currentver/'
+            dest = dirpath                              #iz currentver u previousver
+
+            files = os.listdir(src)                     #kopiramo
             for f in files:
-                if f != "projekat.py" and f!="currentver" and f!="previousver" and f!="logver.txt" and f!="log.py":
-                    shutil.move(src + f, dest)
+                shutil.copy(src + f, dest)
+
+        if x == "selectversion":
+            print("Unesite verziju na koju se zelite prebaciti")
+            b = input()
+            b_save = b
+
+            dirpath = os.path.join('./previousver', b)
+            if not os.path.exists(dirpath):
+                print("Ne mozete se prebaciti na nepostojecu verziju, odnosno onu u kojoj je vracena prethodna verzija!")
+                continue
+
+            b = b + "/"                                 #polaz - folder izabrane verzije
+            src = dirpath                               #destinacija - currentver folder
+            dest = './currentver/'
+
+            for f in os.listdir(dest):                  #brisemo sve iz destinacije pre kopiranja
+                os.remove(os.path.join(dest, f))
+
+            files = os.listdir(src)                     #kopiramo
+            for f in files:
+                shutil.copy(src + f, dest)
+            
+            a = "Reverted to " + b_save + ". version"
+            os.chdir('./vercontrol')
+            log.log_it(a)
+
+            path_parent = os.path.dirname(os.getcwd())  #vracanje jedan dir iznad
+            os.chdir(path_parent)                       #vracanje jedan dir iznad
+            
 
     print("kraj programa")
 
