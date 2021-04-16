32ada78286cc3b63fc0e0d141fdf88622a1d1bbc
Milica <mihailovic.m.business@gmail.com>
2021-04-16 23:15:09 +0200

    Update 5 - GUI update
    Sredjivala i doteravala GUI

--- vercontrol/GUI.py
+++ vercontrol/GUI.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkmb
 
-
-
 def start_project():
    tkmb.showinfo("Repository", "Welcome to your Repository")
 
@@ -11,15 +9,17 @@ def main():
         def __init__(self):
             super().__init__()
             self.title("VCS - GIT")
-            label_1 = tk.Label(self, text="Welcome to GIT!")
-            label_1.pack(fill=tk.BOTH, expand=1, padx=100, pady=15)
-            push_button = tk.Button(text ="Push command", command = start_project)
+            label_1 = tk.Label(self, text="Welcome to GIT!", font=("Helvetica", 50, 'bold', 'italic'), fg="#1b095c", bg="#f2e15c")
+            label_1.pack(fill=tk.BOTH, expand=1, padx=25, pady=15)
+            push_button = tk.Button(text ="Push command", command = start_project, font=("Courier", 25, 'bold', 'underline'), fg="#1b095c", activebackground="grey", activeforeground="blue")
             push_button.pack(side=tk.LEFT, expand=1, padx=25, pady=25)
-            log_button = tk.Button(text ="Log command", command = start_project)
+            log_button = tk.Button(text ="Log command", command = start_project, font=("Courier", 25, 'bold', 'underline'), fg="#1b095c", activebackground="grey", activeforeground="blue")
             log_button.pack(side=tk.LEFT, expand=1, padx=25, pady=25)
-            select_version_button = tk.Button(text ="Select version command", command = start_project)
+            select_version_button = tk.Button(text ="Select version command", command = start_project, font=("Courier", 25, 'bold', 'underline'), fg="#1b095c", activebackground="grey", activeforeground="blue")
             select_version_button.pack(side=tk.RIGHT, expand=1, padx=25, pady=25)
     window = Window()
+    window.minsize(1250, 500)
+    window.configure(bg='#f2e15c')
     window.mainloop()
 if __name__ == "__main__":
     main()
